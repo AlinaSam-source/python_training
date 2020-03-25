6f20e4d3ff4da87d594c6345ffc9519bc8db5b09
ValkSam <do110473sva@gmail.com>
2020-03-25 17:33:39 +0200

    HW

--- test_new_contact.py
+++ test_new_contact.py
@@ -20,7 +20,7 @@ class AddNewContact(unittest.TestCase):
     def test_add_new_contact(self):
         wd = self.wd
         self.open_homepage(wd)
-        self.login(wd, username="admin", passwword="secret")
+        self.login(wd, username="admin", password="secret")
         self.create_group(wd, Contact(firstName="Alina", middlename="Alina", lastname="Alina", nickname="Alina", title="Alina", company="Infopulse", address="Polyova str 24D", home="123", mobile="123",
                           work="123", fax="123", email="1234", email2="1234", email3="1234", homepage="1234", bday="18", bmonth="September", byear="1996", aday="18", amonth="August",
                           ayear="1996", new_group="Alina", address2="1", phone2="1", notes="1"))
@@ -123,16 +123,14 @@ class AddNewContact(unittest.TestCase):
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
-    def login(self, wd, username, passwword):
-        # login
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_xpath("//form[@id='LoginForm']/label[2]").click()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_id("LoginForm").click()
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(passwword)
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_homepage(self, wd):
